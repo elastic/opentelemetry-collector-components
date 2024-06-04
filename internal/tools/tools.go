4d9db88739ca35df6fbcc90041d7d71260ed5bf2
Roger Coll <roger.coll@elastic.co>
2024-06-04 10:03:24 +0200

    add pipeline Go checks (#5)
    * add goproto and govulncheck tools
    
    * ci: add go tests for submodules
    
    * ci: add missing setup-environment stage
    
    * fix: install porto on usage

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -1,6 +1,6 @@
 //go:build tools
 
-package tools
+package tools // import "github.com/elastic/opentelemetry-collector-components/internal/tools"
 
 // This file follows the recommendation at
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
@@ -10,5 +10,7 @@ package tools
 import (
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/jcchavezs/porto/cmd/porto"
 	_ "go.opentelemetry.io/collector/cmd/mdatagen"
+	_ "golang.org/x/vuln/cmd/govulncheck"
 )
