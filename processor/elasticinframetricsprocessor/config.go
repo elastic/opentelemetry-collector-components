cecfd891f9608544d0a4b52a6215364300f09b38
ishleenk17 <ishleen.kaur@elastic.co>
2024-06-05 23:14:55 +0530

    make porto changes to fix CI

--- processor/elasticinframetricsprocessor/config.go
+++ processor/elasticinframetricsprocessor/config.go
@@ -1,4 +1,4 @@
-package elasticprocessor
+package elasticprocessor // import "github.com/elastic/opentelemetry-collector-components/processor/elasticprocessor"
 
 type Config struct {
 	AddSystemMetrics bool `mapstructure:"add_system_metrics"`
--- processor/elasticinframetricsprocessor/factory.go
+++ processor/elasticinframetricsprocessor/factory.go
@@ -1,4 +1,4 @@
-package elasticprocessor
+package elasticprocessor // import "github.com/elastic/opentelemetry-collector-components/processor/elasticprocessor"
 
 import (
 	"context"
