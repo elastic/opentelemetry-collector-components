c0c49f1045cee42d41dce6f95e5e137d25014bdd
Roger Coll <rogercoll@protonmail.com>
2025-01-17 16:40:37 +0100

    chore: add go module references

--- internal/agentcfg/model.go
+++ internal/agentcfg/model.go
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-package agentcfg
+package agentcfg // import "github.com/elastic/opentelemetry-collector-components/internal/agentcfg"
 
 import (
 	"encoding/json"
