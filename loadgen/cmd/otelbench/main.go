1faf18233ab1efbc35ea98eb90c30a18e5cc9b4e
Carson Ip <carson.ip@elastic.co>
2025-01-22 14:38:24 +0000

    Refactor

--- loadgen/cmd/otelbench/main.go
+++ loadgen/cmd/otelbench/main.go
@@ -56,6 +56,53 @@ func fullBenchmarkName(signal, exporter string, concurrency int) string {
 	return fmt.Sprintf("%s-%s-%d", signal, exporter, concurrency)
 }
 
+func runBench(signal, exporter string, concurrency int) testing.BenchmarkResult {
+	return testing.Benchmark(func(b *testing.B) {
+		// loadgenreceiver will send stats about generated telemetry when it finishes sending b.N iterations
+		logsDone := make(chan loadgenreceiver.Stats)
+		metricsDone := make(chan loadgenreceiver.Stats)
+		tracesDone := make(chan loadgenreceiver.Stats)
+		if signal != "logs" {
+			close(logsDone)
+		}
+		if signal != "metrics" {
+			close(metricsDone)
+		}
+		if signal != "traces" {
+			close(tracesDone)
+		}
+
+		stop := make(chan struct{}) // close channel to stop the loadgen collector
+
+		go func() {
+			logsStats := <-logsDone
+			metricsStats := <-metricsDone
+			tracesStats := <-tracesDone
+			b.StopTimer()
+
+			stats := logsStats.Add(metricsStats).Add(tracesStats)
+
+			elapsedSeconds := b.Elapsed().Seconds()
+			b.ReportMetric(float64(stats.LogRecords)/elapsedSeconds, "logs/s")
+			b.ReportMetric(float64(stats.MetricDataPoints)/elapsedSeconds, "metric_points/s")
+			b.ReportMetric(float64(stats.Spans)/elapsedSeconds, "spans/s")
+			b.ReportMetric(float64(stats.Requests)/elapsedSeconds, "requests/s")
+			b.ReportMetric(float64(stats.FailedLogRecords)/elapsedSeconds, "failed_logs/s")
+			b.ReportMetric(float64(stats.FailedMetricDataPoints)/elapsedSeconds, "failed_metric_points/s")
+			b.ReportMetric(float64(stats.FailedSpans)/elapsedSeconds, "failed_spans/s")
+			b.ReportMetric(float64(stats.FailedRequests)/elapsedSeconds, "failed_requests/s")
+			// TODO(carsonip): optionally retrieve metrics (e.g. memory, cpu) of target server from Elasticsearch
+
+			close(stop)
+		}()
+
+		err := RunCollector(context.Background(), stop, configs(exporter, signal, b.N, concurrency), logsDone, metricsDone, tracesDone)
+		if err != nil {
+			b.Fatal(err)
+		}
+	})
+}
+
 func main() {
 	testing.Init()
 	if err := Init(); err != nil {
@@ -80,50 +127,7 @@ func main() {
 		for _, signal := range getSignals() {
 			for _, exporter := range getExporters() {
 				benchName := fullBenchmarkName(signal, exporter, concurrency)
-				result := testing.Benchmark(func(b *testing.B) {
-					// loadgenreceiver will send stats about generated telemetry when it finishes sending b.N iterations
-					logsDone := make(chan loadgenreceiver.Stats)
-					metricsDone := make(chan loadgenreceiver.Stats)
-					tracesDone := make(chan loadgenreceiver.Stats)
-					if signal != "logs" {
-						close(logsDone)
-					}
-					if signal != "metrics" {
-						close(metricsDone)
-					}
-					if signal != "traces" {
-						close(tracesDone)
-					}
-
-					stop := make(chan struct{}) // close channel to stop the loadgen collector
-
-					go func() {
-						logsStats := <-logsDone
-						metricsStats := <-metricsDone
-						tracesStats := <-tracesDone
-						b.StopTimer()
-
-						stats := logsStats.Add(metricsStats).Add(tracesStats)
-
-						elapsedSeconds := b.Elapsed().Seconds()
-						b.ReportMetric(float64(stats.LogRecords)/elapsedSeconds, "logs/s")
-						b.ReportMetric(float64(stats.MetricDataPoints)/elapsedSeconds, "metric_points/s")
-						b.ReportMetric(float64(stats.Spans)/elapsedSeconds, "spans/s")
-						b.ReportMetric(float64(stats.Requests)/elapsedSeconds, "requests/s")
-						b.ReportMetric(float64(stats.FailedLogRecords)/elapsedSeconds, "failed_logs/s")
-						b.ReportMetric(float64(stats.FailedMetricDataPoints)/elapsedSeconds, "failed_metric_points/s")
-						b.ReportMetric(float64(stats.FailedSpans)/elapsedSeconds, "failed_spans/s")
-						b.ReportMetric(float64(stats.FailedRequests)/elapsedSeconds, "failed_requests/s")
-						// TODO(carsonip): optionally retrieve metrics (e.g. memory, cpu) of target server from Elasticsearch
-
-						close(stop)
-					}()
-
-					err := RunCollector(context.Background(), stop, configs(exporter, signal, b.N, concurrency), logsDone, metricsDone, tracesDone)
-					if err != nil {
-						b.Fatal(err)
-					}
-				})
+				result := runBench(signal, exporter, concurrency)
 				// write benchmark result to stdout, as stderr may be cluttered with collector logs
 				fmt.Printf("%-*s\t%s\n", maxLen, benchName, result.String())
 			}
