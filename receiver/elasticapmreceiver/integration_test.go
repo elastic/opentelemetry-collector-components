92a99c9d8407cc2dfb4b978e5be0f6d50bc3fd0c
Roger Coll <rogercoll@protonmail.com>
2025-02-21 09:59:54 +0100

    test: eventually write to es index

--- receiver/elasticapmreceiver/integration_test.go
+++ receiver/elasticapmreceiver/integration_test.go
@@ -220,8 +220,11 @@ func apmConfigintegrationTest(name string) func(t *testing.T) {
 			},
 		})
 		require.NoError(t, err)
-		err = createApmConfigIndex(esClient, esEndpoint)
-		require.NoError(t, err)
+
+		// ES writes might are not immediately available
+		assert.Eventually(t, func() bool {
+			return assert.NoError(t, createApmConfigIndex(esClient, esEndpoint))
+		}, 1*time.Minute, 1*time.Second)
 
 		rcvrFactory := NewFactory()
 		cfg := &Config{
