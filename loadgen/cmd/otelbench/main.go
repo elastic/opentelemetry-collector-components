213ce72d4f321922e02ad7a206230d14d53b1ee6
Constanca Manteigas <constanca.manteigas@elastic.co>
2025-03-03 09:17:26 +0100

    Add println on error

--- loadgen/cmd/otelbench/main.go
+++ loadgen/cmd/otelbench/main.go
@@ -98,6 +98,7 @@ func runBench(signal, exporter string, concurrency int) testing.BenchmarkResult 
 
 		err := RunCollector(context.Background(), stop, configs(exporter, signal, b.N, concurrency), logsDone, metricsDone, tracesDone)
 		if err != nil {
+			fmt.Println(err.Error())
 			b.Fatal(err)
 		}
 	})
