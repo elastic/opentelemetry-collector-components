1ab52e9a30e1bcb0560ee5363ef22068766474b4
Constanca Manteigas <constanca.manteigas@elastic.co>
2025-03-04 10:45:41 +0100

    Open PR

---
+++ .github/workflows/scripts/create_pr.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -eo pipefail
+
+VERSION=$1
+GITHUB_EVENT_NAME=$2
+BRANCH_NAME="update-changelog-$VERSION"
+
+(
+  cd ../../../loadgen/cmd/otelbench
+  ./chloggen update --version $VERSION
+
+  if [ "$GITHUB_EVENT_NAME" = "workflow_dispatch" ]; then
+    # update version inside Makefile
+    sed -i "s/^VERSION := .*/VERSION := $VERSION/" Makefile
+  fi
+)
+
+git checkout -b "$BRANCH_NAME"
+git add ./loadgen/cmd/otelbench
+if ! git diff --quiet ./loadgen/cmd/otelbench; then
+  git commit -m "Update otelbench changelog and delete changelog fragments."
+  git push origin "$BRANCH_NAME"
+else
+  echo "No changes to commit."
+  exit 0
+fi
+
+echo "Create PR"
+gh pr create \
+  --title="[loadgen/otelbench] Update changelog for $VERSION" \
+  --base "main" \
+  --head "$BRANCH_NAME" \
+  -b "This PR updates the changelog and removes old changelog fragments."
