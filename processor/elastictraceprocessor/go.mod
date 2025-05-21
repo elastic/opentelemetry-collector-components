37a231e1fbb6cf665ca4434a61b1bdc6ee126e40
girodav <1390902+girodav@users.noreply.github.com>
2025-05-21 13:30:19 +0100

    First testing

--- processor/elastictraceprocessor/go.mod
+++ processor/elastictraceprocessor/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/open-telemetry/opentelemetry-collector-contrib/pkg/golden v0.124.1
 	github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest v0.124.1
 	github.com/stretchr/testify v1.10.0
+	go.opentelemetry.io/collector/client v1.28.1
 	go.opentelemetry.io/collector/component v1.30.0
 	go.opentelemetry.io/collector/component/componenttest v0.124.0
 	go.opentelemetry.io/collector/confmap v1.30.0
@@ -15,6 +16,7 @@ require (
 	go.opentelemetry.io/collector/pdata v1.30.0
 	go.opentelemetry.io/collector/processor v1.30.0
 	go.opentelemetry.io/collector/processor/processortest v0.124.0
+	go.opentelemetry.io/collector/semconv v0.124.0
 	go.uber.org/goleak v1.3.0
 	go.uber.org/zap v1.27.0
 )
@@ -49,7 +51,6 @@ require (
 	go.opentelemetry.io/collector/pdata/testdata v0.124.0 // indirect
 	go.opentelemetry.io/collector/pipeline v0.124.0 // indirect
 	go.opentelemetry.io/collector/processor/xprocessor v0.124.0 // indirect
-	go.opentelemetry.io/collector/semconv v0.124.0 // indirect
 	go.opentelemetry.io/contrib/bridges/otelzap v0.10.0 // indirect
 	go.opentelemetry.io/otel v1.35.0 // indirect
 	go.opentelemetry.io/otel/log v0.11.0 // indirect
--- processor/elastictraceprocessor/processor.go
+++ processor/elastictraceprocessor/processor.go
@@ -19,13 +19,18 @@ package elastictraceprocessor // import "github.com/elastic/opentelemetry-collec
 
 import (
 	"context"
-
 	"github.com/elastic/opentelemetry-lib/enrichments/trace"
+	"go.opentelemetry.io/collector/client"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/ptrace"
 	"go.opentelemetry.io/collector/processor"
+	semconv26 "go.opentelemetry.io/collector/semconv/v1.26.0"
+	semconv "go.opentelemetry.io/collector/semconv/v1.27.0"
 	"go.uber.org/zap"
+	"strings"
+	"time"
 )
 
 var _ processor.Traces = (*Processor)(nil)
@@ -52,6 +57,150 @@ func (p *Processor) Capabilities() consumer.Capabilities {
 }
 
 func (p *Processor) ConsumeTraces(ctx context.Context, td ptrace.Traces) error {
+
+	resourceSpans := td.ResourceSpans()
+	for i := 0; i < resourceSpans.Len(); i++ {
+		resourceSpan := resourceSpans.At(i)
+		resource := resourceSpan.Resource()
+		translateResourceMetadata(resource)
+		attributes := resource.Attributes()
+
+		serviceName, _ := attributes.Get("service.name")
+
+		dataStreamType := "logs" // Default to traces; adjust based on logic
+
+		attributes.PutStr("data_stream.type", dataStreamType)
+		attributes.PutStr("data_stream.dataset", "apm."+serviceName.AsString())
+		attributes.PutStr("data_stream.namespace", "default") // Use a default or configurable namespace
+
+		scopeSpans := resourceSpan.ScopeSpans()
+
+		for j := 0; j < scopeSpans.Len(); j++ {
+			scopeSpan := scopeSpans.At(j)
+			spans := scopeSpan.Spans()
+
+			for k := 0; k < spans.Len(); k++ {
+				span := spans.At(k)
+				attributes := span.Attributes()
+
+				// Add "@timestamp" attribute
+				timestamp := span.StartTimestamp().AsTime().Format(time.RFC3339Nano)
+				attributes.PutStr("timestamp", timestamp)
+			}
+		}
+	}
 	p.enricher.Enrich(td)
-	return p.next.ConsumeTraces(ctx, td)
+	ecsCtx := client.NewContext(ctx, withMappingMode(client.FromContext(ctx), "ecs"))
+	return p.next.ConsumeTraces(ecsCtx, td)
+}
+
+func withMappingMode(info client.Info, mode string) client.Info {
+	return client.Info{
+		Addr:     info.Addr,
+		Auth:     info.Auth,
+		Metadata: client.NewMetadata(map[string][]string{"x-elastic-mapping-mode": {mode}}),
+	}
+}
+
+func translateResourceMetadata(resource pcommon.Resource) {
+	attributes := resource.Attributes()
+
+	attributes.Range(func(k string, v pcommon.Value) bool {
+		if !isSupportedAttribute(k) {
+			attributes.PutStr("labels."+replaceDots(k), v.AsString())
+			attributes.Remove(k)
+		}
+		return true
+	})
+}
+
+// Helper function to replace dots in attribute keys
+func replaceDots(key string) string {
+	return strings.ReplaceAll(key, ".", "_")
+}
+
+func isSupportedAttribute(attr string) bool {
+	switch attr {
+	// service.*
+	case semconv.AttributeServiceName,
+		semconv.AttributeServiceVersion,
+		semconv.AttributeServiceInstanceID,
+		semconv.AttributeServiceNamespace:
+		return true
+
+	// deployment.*
+	case semconv26.AttributeDeploymentEnvironment, semconv.AttributeDeploymentEnvironmentName:
+		return true
+
+	// telemetry.sdk.*
+	case semconv.AttributeTelemetrySDKName,
+		semconv.AttributeTelemetrySDKVersion,
+		semconv.AttributeTelemetrySDKLanguage:
+		return true
+
+	// cloud.*
+	case semconv.AttributeCloudProvider,
+		semconv.AttributeCloudAccountID,
+		semconv.AttributeCloudRegion,
+		semconv.AttributeCloudAvailabilityZone,
+		semconv.AttributeCloudPlatform:
+		return true
+
+	// container.*
+	case semconv.AttributeContainerName,
+		semconv.AttributeContainerID,
+		semconv.AttributeContainerImageName,
+		"container.image.tag",
+		"container.runtime":
+		return true
+
+	// k8s.*
+	case semconv.AttributeK8SNamespaceName,
+		semconv.AttributeK8SNodeName,
+		semconv.AttributeK8SPodName,
+		semconv.AttributeK8SPodUID:
+		return true
+
+	// host.*
+	case semconv.AttributeHostName,
+		semconv.AttributeHostID,
+		semconv.AttributeHostType,
+		"host.arch",
+		semconv.AttributeHostIP:
+		return true
+
+	// process.*
+	case semconv.AttributeProcessPID,
+		semconv.AttributeProcessCommandLine,
+		semconv.AttributeProcessExecutablePath,
+		"process.runtime.name",
+		"process.runtime.version",
+		semconv.AttributeProcessOwner:
+		return true
+
+	// os.*
+	case semconv.AttributeOSType,
+		semconv.AttributeOSDescription,
+		semconv.AttributeOSName,
+		semconv.AttributeOSVersion:
+		return true
+
+	// device.*
+	case semconv.AttributeDeviceID,
+		semconv.AttributeDeviceModelIdentifier,
+		semconv.AttributeDeviceModelName,
+		"device.manufacturer":
+		return true
+
+	// data_stream.*
+	case "data_stream.dataset",
+		"data_stream.namespace":
+		return true
+
+	// Legacy OpenCensus attributes
+	case "opencensus.exporterversion":
+		return true
+	}
+
+	return false
 }
