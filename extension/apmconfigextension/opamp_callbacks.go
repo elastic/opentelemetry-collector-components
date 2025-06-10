2d33b0370c9c9e96eaece20ed37e7a907b7fdbe7
Trent Mick <trentm@gmail.com>
2025-06-10 16:05:02 -0700

    [extension/apmconfig] save RemoteConfigStatus.LastRemoteConfigHash regardless of status
    Before this change the OpAMP server would only store the LastRemoteConfigHash
    provided in an AgentToServer message if the RemoteConfigStatus.Status
    was "APPLIED".
    
    An agent may want to (a) acknowledge that a particular config was
    received (via LastRemoteConfigHash) *and* (b) provide some other status
    (APPLYING or FAILED). In my particular case, for EDOT Node.js, I want to
    be able to support the case where the server might be sending new config
    names that an older EDOT Node.js might not know about. My
    RemoteConfigStatus in this case is:
      LastRemoteConfigHash: ...
      Status=FAILED
      ErrorMessage="there were issues applying remote config: config name 'some_new_var' is unsupported"
    
    The downside of the current behaviour is that the server will ignore the
    provided LastRemoteConfigHash and continue to re-send the same remote
    config in every ServerToAgent. Re-sending is not going to fix the issue
    the agent had with applying the config.
    
    My interpretation of the OpAMP spec and proto is that the OpAMP server
    should use *only* the LastRemoteConfigHash field for determining if
    RemoteConfig should be sent. The proto says:
    
        // The hash of the remote config that was last received by this Agent in the
        // AgentRemoteConfig.config_hash field.
        // The Server SHOULD compare this hash with the config hash
        // it has for the Agent and if the hashes are different the Server MUST include
        // the remote_config field in the response in the ServerToAgent message.

--- extension/apmconfigextension/opamp_callbacks.go
+++ extension/apmconfigextension/opamp_callbacks.go
@@ -132,11 +132,14 @@ func (rc *remoteConfigCallbacks) onMessage(ctx context.Context, conn types.Conne
 		return &serverToAgent
 	}
 
-	if message.GetRemoteConfigStatus() != nil && message.GetRemoteConfigStatus().Status == protobufs.RemoteConfigStatuses_RemoteConfigStatuses_APPLIED && bytes.Equal(remoteConfig.ConfigHash, message.RemoteConfigStatus.GetLastRemoteConfigHash()) {
-		rc.logger.Info("Remote config applied", zap.String("hash", hex.EncodeToString(remoteConfig.ConfigHash)), agentUidField)
-		agent.lastConfigHash = message.GetRemoteConfigStatus().GetLastRemoteConfigHash()
+	remoteConfigStatus := message.GetRemoteConfigStatus()
+	if remoteConfigStatus != nil {
+		agent.lastConfigHash = remoteConfigStatus.GetLastRemoteConfigHash()
+		rc.logger.Info("Remote config status", agentUidField, zap.String("lastRemoteConfigHash", hex.EncodeToString(agent.lastConfigHash)), zap.String("status", remoteConfigStatus.GetStatus().String()), zap.String("errorMessage", remoteConfigStatus.ErrorMessage))
 		rc.agentState.Store(agentUid, agent)
-	} else if !bytes.Equal(agent.lastConfigHash, remoteConfig.ConfigHash) {
+	}
+
+	if !bytes.Equal(agent.lastConfigHash, remoteConfig.ConfigHash) {
 		rc.logger.Info("Sending new remote configuration", agentUidField, zap.String("hash", hex.EncodeToString(remoteConfig.ConfigHash)))
 		serverToAgent.RemoteConfig = remoteConfig
 	}
