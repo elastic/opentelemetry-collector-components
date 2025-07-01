aba3ca3ede7d3adb58c579e395bdccb8b7cd346e
Greg Kalapos <gergo@kalapos.net>
2025-07-01 11:51:52 +0200

    run porto

--- processor/elasticapmprocessor/factory.go
+++ processor/elasticapmprocessor/factory.go
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-package elasticapmprocessor // import "github.com/elastic/opentelemetry-collector-components/processor/elasticapmprocessor"
+package elasticapmprocessor // import "github.com/elastic/opentelemetry-collector-components/processor/elastictraceprocessor"
 
 import (
 	"context"
