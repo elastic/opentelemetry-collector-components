06010888564cab9b65f3837494ed26651b50adf1
Davide Girardi <1390902+girodav@users.noreply.github.com>
2025-07-10 08:14:39 +0100

    [receiver/elasticapmintakereceiver] fix wrong metadata type and README (#652)

--- receiver/elasticapmintakereceiver/generated_component_test.go
+++ receiver/elasticapmintakereceiver/generated_component_test.go
@@ -32,7 +32,7 @@ import (
 	"go.opentelemetry.io/collector/receiver/receivertest"
 )
 
-var typ = component.MustNewType("elasticapm")
+var typ = component.MustNewType("elasticapmintake")
 
 func TestComponentFactoryType(t *testing.T) {
 	require.Equal(t, typ, NewFactory().Type())
