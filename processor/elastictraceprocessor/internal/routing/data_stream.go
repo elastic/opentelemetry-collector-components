c4d2e97d195a98c487f170c506925a77c63cec6a
Greg Kalapos <gergo@kalapos.net>
2025-07-16 14:29:58 +0200

    Adding back elastictrace processor for smooth deprecation

---
+++ processor/elastictraceprocessor/internal/routing/data_stream.go
@@ -0,0 +1,28 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package routing // import "github.com/elastic/opentelemetry-collector-components/processor/elastictraceprocessor/internal/routing"
+
+import "go.opentelemetry.io/collector/pdata/pcommon"
+
+func EncodeDataStream(resource pcommon.Resource, dataStreamType string) {
+	attributes := resource.Attributes()
+
+	attributes.PutStr("data_stream.type", dataStreamType)
+	attributes.PutStr("data_stream.dataset", "apm")
+	attributes.PutStr("data_stream.namespace", "default") //TODO: make this configurable
+}
