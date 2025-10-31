5164bfbb6034d018143d1663866a68f89eef702e
Isaac Flores <isaac.flores@elastic.co>
2025-10-31 15:20:13 -0700

    Change `process.args` from joined string to array. This avoids having to unsplit the string at export time

--- receiver/elasticapmintakereceiver/internal/mappers/intakeV2ToSemConv.go
+++ receiver/elasticapmintakereceiver/internal/mappers/intakeV2ToSemConv.go
@@ -222,7 +222,11 @@ func translateProcessUserNetworkAttributes(event *modelpb.APMEvent, attributes p
 			attributes.PutStr(string(semconv.ProcessExecutableNameKey), event.Process.Title)
 		}
 		if len(event.Process.Argv) > 0 {
-			attributes.PutStr(string(semconv.ProcessCommandLineKey), strings.Join(event.Process.Argv, " "))
+			commandLineArgs := attributes.PutEmptySlice(string(semconv.ProcessCommandLineKey))
+			commandLineArgs.EnsureCapacity(len(event.Process.Argv))
+			for _, arg := range event.Process.Argv {
+				commandLineArgs.AppendEmpty().SetStr(arg)
+			}
 		}
 		if event.Process.Executable != "" {
 			attributes.PutStr(string(semconv.ProcessExecutablePathKey), event.Process.Executable)
