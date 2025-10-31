87d9306804519354dd4159b62707d5c9988faf7e
vigneshshanmugam <vignesh.shanmugam22@gmail.com>
2025-10-30 21:41:48 -0700

    fix go generate

--- processor/ratelimitprocessor/internal/metadatatest/generated_telemetrytest_test.go
+++ processor/ratelimitprocessor/internal/metadatatest/generated_telemetrytest_test.go
@@ -27,6 +27,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric/metricdata"
 	"go.opentelemetry.io/otel/sdk/metric/metricdata/metricdatatest"
 
+	"github.com/elastic/opentelemetry-collector-components/processor/ratelimitprocessor/internal/metadata"
 	"go.opentelemetry.io/collector/component/componenttest"
 )
 
