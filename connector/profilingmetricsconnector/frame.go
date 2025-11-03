cda0f67606701c9d985b906b8e74ed1c9d4fdca9
Christos Kalkanis <christos.kalkanis@elastic.co>
2025-11-03 09:01:34 -0500

    profilingmetricsconnector: Add kernel stacktrace classification

--- connector/profilingmetricsconnector/frame.go
+++ connector/profilingmetricsconnector/frame.go
@@ -20,6 +20,7 @@ package profilingmetricsconnector // import "github.com/elastic/opentelemetry-co
 import (
 	"fmt"
 	"regexp"
+	"strings"
 
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
@@ -34,11 +35,25 @@ type metric struct {
 }
 
 type frameInfo struct {
-	typ      string
-	filename string
+	typ          string
+	fileName     string
+	functionName string
 }
 
-const nativeLibraryAttrName = "shlib_name"
+type class struct {
+	name string
+	rx   *regexp.Regexp
+}
+
+const (
+	nativeLibraryAttrName = "shlib_name"
+)
+
+var kernelClassAttrNames = [...]string{
+	"kernel_0",
+	"kernel_1",
+	"kernel_2",
+}
 
 var (
 	metricUser   = metric{name: "samples.user.count", desc: "Number of samples executing userspace code (self)"}
@@ -70,7 +85,47 @@ var (
 		frameTypeBeam:   metricBeam,
 	}
 
-	rx = regexp.MustCompile(`(?:.*/)?(.+)\.so`)
+	shlibRx = regexp.MustCompile(`(?:.*/)?(.+)\.so`)
+
+	// The following regular expressions try to match certain kernel functions
+	// and derive a classification for an entire kernel stacktrace based on the match.
+	// The ordering is *significant* as some of the following functions may appear
+	// in stacktraces that result in multiple classifications *for the same*
+	// stacktrace which would be incorrect. The elements are ordered in decreasing
+	// priority: if there are multiple classifications, the element with the lower
+	// slice index always wins.
+	classes = []class{
+		class{name: "network/tcp/read",
+			rx: regexp.MustCompile(`^tcp_recvmsg|^tcp_read|^tcp_rcv|^tcp_v4_rcv|^tcp_v6_rcv`)},
+		class{name: "network/tcp/write",
+			rx: regexp.MustCompile(`^tcp_sendmsg|^tcp_transmit|^tcp_write|^tcp_push|^tcp_sendpage`)},
+		class{name: "network/udp/read",
+			rx: regexp.MustCompile(`^udp_recvmsg|^udp_read|^udp_rcv|^udp_v4_rcv|^udp_v6_rcv`)},
+		class{name: "network/udp/write",
+			rx: regexp.MustCompile(`^udp_sendmsg|^udp_write|^udp_push|^udp_sendpage`)},
+		class{name: "ipc/read",
+			rx: regexp.MustCompile(`^pipe_read|^pipe_read_iter|^eventfd_read` +
+				`|^unix_stream_read_generic|^unix_stream_recvmsg`)},
+		class{name: "ipc/write",
+			rx: regexp.MustCompile(`^pipe_write|^pipe_write_iter|^eventfd_write` +
+				`|^unix_stream_write_generic|^unix_stream_sendmsg`)},
+		class{name: "network/other/read",
+			rx: regexp.MustCompile(`^sock_recvmsg|^__sock_recvmsg|^sock_read_iter`)},
+		class{name: "network/other/write",
+			rx: regexp.MustCompile(`^sock_sendmsg|^__sock_sendmsg|^sock_write_iter`)},
+		class{name: "disk/read",
+			rx: regexp.MustCompile(`^ext4_file_read_iter|^xfs_file_read_iter` +
+				`|^btrfs_file_read_iter|^filemap_read|^generic_file_read_iter`)},
+		class{name: "disk/write",
+			rx: regexp.MustCompile(`^ext4_file_write_iter|^xfs_file_write_iter` +
+				`|^btrfs_file_write_iter|^filemap_write|^generic_file_write_iter` +
+				`|^writeback_sb_inodes`)},
+		class{name: "memory",
+			rx: regexp.MustCompile(`^do_mmap|do_munmap|^do_anonymous_page|mmap_|madvise` +
+				`|^handle_mm_fault|^alloc_pages|^free_pages`)},
+		class{name: "synchronization",
+			rx: regexp.MustCompile(`futex_|^schedule|__schedule|^wake_up_|^wake_q_`)},
+	}
 )
 
 func fetchFrameInfo(dictionary pprofile.ProfilesDictionary,
@@ -78,36 +133,39 @@ func fetchFrameInfo(dictionary pprofile.ProfilesDictionary,
 	sampleLocationIndex int,
 ) (frameInfo, error) {
 	attrTable := dictionary.AttributeTable()
-	locationTable := dictionary.LocationTable()
+	locTable := dictionary.LocationTable()
 	strTable := dictionary.StringTable()
+	funcTable := dictionary.FunctionTable()
 
-	strLen := strTable.Len()
-
+	strTbLen := strTable.Len()
+	locTbLen := locTable.Len()
+	attrTbLen := attrTable.Len()
+	funcTbLen := funcTable.Len()
 	liLen := locationIndices.Len()
+
 	if sampleLocationIndex >= liLen {
 		return frameInfo{}, fmt.Errorf("fetchFrameInfo: sli %d >= %d",
 			sampleLocationIndex, liLen)
 	}
 
 	li := locationIndices.At(sampleLocationIndex)
-	ltLen := locationTable.Len()
-	if int(li) >= ltLen {
+	if int(li) >= locTbLen {
 		return frameInfo{}, fmt.Errorf("fetchFrameInfo: li %d >= %d",
-			li, ltLen)
+			li, locTbLen)
 	}
 
-	leaf := locationTable.At(int(li))
+	loc := locTable.At(int(li))
 	frameType := ""
-	aLen := attrTable.Len()
-	for _, idx := range leaf.AttributeIndices().All() {
-		if int(idx) >= aLen {
+
+	for _, idx := range loc.AttributeIndices().All() {
+		if int(idx) >= attrTbLen {
 			return frameInfo{}, fmt.Errorf("fetchFrameInfo: ai %d >= %d",
-				idx, aLen)
+				idx, attrTbLen)
 		}
 		attr := attrTable.At(int(idx))
-		if int(attr.KeyStrindex()) >= strLen {
+		if int(attr.KeyStrindex()) >= strTbLen {
 			return frameInfo{}, fmt.Errorf("fetchFrameInfo: attr.KeyStrindex %d >= %d",
-				attr.KeyStrindex(), strLen)
+				attr.KeyStrindex(), strTbLen)
 		}
 
 		if strTable.At(int(attr.KeyStrindex())) == string(semconv.ProfileFrameTypeKey) {
@@ -123,35 +181,103 @@ func fetchFrameInfo(dictionary pprofile.ProfilesDictionary,
 		return frameInfo{}, fmt.Errorf("fetchFrameInfo: empty frame type")
 	}
 
-	var fname string
-	if frameType == frameTypeNative {
+	fileName := ""
+	funcName := ""
+
+	switch frameType {
+	case frameTypeNative:
 		// Extract mapping filename
-		mappingTable := dictionary.MappingTable()
-		mtLen := mappingTable.Len()
-		mi := leaf.MappingIndex()
-		if int(mi) >= mtLen {
-			return frameInfo{}, fmt.Errorf("fetchFrameInfo: mi %d >= %d",
-				mi, mtLen)
+		mapTable := dictionary.MappingTable()
+		mapTbLen := mapTable.Len()
+		mi := loc.MappingIndex()
+		if int(mi) >= mapTbLen {
+			// TODO: log error
+			// return frameInfo{}, fmt.Errorf("fetchFrameInfo: mi %d >= %d",
+			//	mi, mapTbLen)
+			break
 		}
-		mapFilenameStri := int(mappingTable.At(int(mi)).FilenameStrindex())
-		if mapFilenameStri >= strLen {
-			return frameInfo{}, fmt.Errorf("fetchFrameInfo: mapping.FilenameStridex %d >= %d",
-				mapFilenameStri, strLen)
+		mapFilenameStri := int(mapTable.At(int(mi)).FilenameStrindex())
+		if mapFilenameStri >= strTbLen {
+			// TODO: log error
+			// return frameInfo{}, fmt.Errorf("fetchFrameInfo: mapping.FilenameStridex %d >= %d",
+			//	mapFilenameStri, strTbLen)
+			break
 		}
-		fname = strTable.At(mapFilenameStri)
+		fileName = strTable.At(mapFilenameStri)
+	case frameTypeKernel:
+		// Extract function name
+		for _, ln := range loc.Line().All() {
+			if ln.FunctionIndex() >= int32(funcTbLen) {
+				// TODO: log error
+				continue
+			}
+			fn := funcTable.At(int(ln.FunctionIndex()))
+			if fn.NameStrindex() >= int32(strTbLen) {
+				// TODO: log error
+				continue
+			}
+			funcName = strTable.At(int(fn.NameStrindex()))
+		}
 	}
 
 	// End validation
-	return frameInfo{typ: frameType, filename: fname}, nil
+	return frameInfo{
+		typ:          frameType,
+		fileName:     fileName,
+		functionName: funcName}, nil
 }
 
-// classifyFrame classifies sample into one or more categories based on frame type.
-// This takes place by incrementing the associated metric count.
-func classifyFrame(dictionary pprofile.ProfilesDictionary,
+func classifyKernelFrames(dictionary pprofile.ProfilesDictionary,
 	locationIndices pcommon.Int32Slice,
-	sample pprofile.Sample,
+	counts map[metric]int64,
+	kernelCounts map[string]int64,
+) error {
+	var err error
+
+	className := ""
+	lastMatchIdx := len(classes)
+	for idx := range locationIndices.Len() {
+		// Iterate over all kernel frames
+		fi, err := fetchFrameInfo(dictionary, locationIndices, idx)
+		if err != nil || fi.typ != frameTypeKernel {
+			break
+		}
+
+		if fi.functionName == "" {
+			// No classification possible
+			continue
+		}
+
+		for cIdx, class := range classes {
+			if cIdx == lastMatchIdx {
+				// Early exit if there's no higher priority match possible
+				break
+			}
+			if class.rx.MatchString(fi.functionName) {
+				className = class.name
+				lastMatchIdx = cIdx
+				break
+			}
+		}
+	}
+
+	if className == "" {
+		// No kernel category match found
+		counts[metricKernel]++
+	} else {
+		kernelCounts[className]++
+	}
+
+	return err
+}
+
+// classifyFrames classifies a sample into one or more categories based on frame information.
+// This takes place by incrementing the associated metric count.
+func classifyFrames(dictionary pprofile.ProfilesDictionary,
+	locationIndices pcommon.Int32Slice,
 	counts map[metric]int64,
 	nativeCounts map[string]int64,
+	kernelCounts map[string]int64,
 ) error {
 	fi, err := fetchFrameInfo(dictionary, locationIndices, 0)
 	if err != nil {
@@ -166,23 +292,25 @@ func classifyFrame(dictionary pprofile.ProfilesDictionary,
 	// TODO: Scale all counts by number of events in each Sample. Currently,
 	// this logic assumes 1 event per Sample (thus the increments by 1 below),
 	// which isn't necessarily the case.
-
 	if leafFrameType != frameTypeKernel {
 		counts[metricUser]++
 	}
 
-	if leafFrameType != frameTypeNative {
-		counts[metric]++
-		return nil
+	if leafFrameType == frameTypeNative && fi.fileName != "" {
+		// Extract native library name and increment associated count
+		if sm := shlibRx.FindStringSubmatch(fi.fileName); sm != nil {
+			nativeCounts[sm[1]]++
+			return nil
+		}
 	}
 
-	// Extract native library name and increment associated count
-	if sm := rx.FindStringSubmatch(fi.filename); sm != nil {
-		nativeCounts[sm[1]]++
-	} else {
-		counts[metric]++
+	if leafFrameType == frameTypeKernel {
+		return classifyKernelFrames(dictionary,
+			locationIndices,
+			counts, kernelCounts)
 	}
 
+	counts[metric]++
 	return nil
 }
 
@@ -193,12 +321,13 @@ func (c *profilesToMetricsConnector) addFrameMetrics(dictionary pprofile.Profile
 
 	counts := make(map[metric]int64)
 	nativeCounts := make(map[string]int64)
+	kernelCounts := make(map[string]int64)
 
 	// Process all samples and extract metric counts
 	for _, sample := range profile.Sample().All() {
 		stack := stackTable.At(int(sample.StackIndex()))
-		if err := classifyFrame(dictionary, stack.LocationIndices(),
-			sample, counts, nativeCounts); err != nil {
+		if err := classifyFrames(dictionary, stack.LocationIndices(),
+			counts, nativeCounts, kernelCounts); err != nil {
 			// Should not happen with well-formed profile data
 			// TODO: Add error metric or log error
 			continue
@@ -235,5 +364,26 @@ func (c *profilesToMetricsConnector) addFrameMetrics(dictionary pprofile.Profile
 		dp.SetTimestamp(profile.Time())
 		dp.SetIntValue(count)
 		dp.Attributes().PutStr(nativeLibraryAttrName, libraryName)
+	}
+
+	for className, count := range kernelCounts {
+		m := scopeMetrics.Metrics().AppendEmpty()
+		m.SetName(c.config.MetricsPrefix + metricKernel.name)
+		m.SetDescription(metricKernel.desc)
+		m.SetUnit("1")
+
+		sum := m.SetEmptySum()
+		sum.SetIsMonotonic(true)
+		sum.SetAggregationTemporality(pmetric.AggregationTemporalityDelta)
+
+		dp := sum.DataPoints().AppendEmpty()
+		dp.SetTimestamp(profile.Time())
+		dp.SetIntValue(count)
+
+		for i, name := range strings.Split(className, "/") {
+			if name != "" {
+				dp.Attributes().PutStr(kernelClassAttrNames[i], name)
+			}
+		}
 	}
 }
