16ca9e81662d9fb779212199d71034a64f6a88f7
Isaac Flores <isaac.flores@elastic.co>
2025-12-08 11:31:42 -0800

    Run `make goporto`

--- extension/clientaddrmiddlewareextension/middleware.go
+++ extension/clientaddrmiddlewareextension/middleware.go
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-package clientaddrmiddlewareextension
+package clientaddrmiddlewareextension // import "github.com/elastic/opentelemetry-collector-components/extension/clientaddrmiddlewareextension"
 
 import (
 	"context"
